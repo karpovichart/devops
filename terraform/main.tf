ceff764a8d5224951d1f821da99c23e7555d25b5
xbxjsnsb <adverterio@gmail.com>
2020-04-01 18:14:41 +0300

    added SSH keys generation for connecting to each resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,7 @@ resource "aws_instance" "ci_cd_server"{
     ami           = "ami-0b418580298265d5c"
     instance_type = "t2.micro"
     security_groups = [ aws_security_group.security_group.name ]
+    key_name = aws_key_pair.key_pair_ci_cd_server.key_name
 	  user_data = <<EOF
 #!/bin/bash
 sudo apt update
@@ -35,6 +36,7 @@ resource "aws_instance" "wordpress_server"{
     ami           = "ami-0b418580298265d5c"
     instance_type = "t2.micro"
     security_groups = [ aws_security_group.security_group.name ]
+    key_name = aws_key_pair.key_pair_wordpress_server.key_name
     tags = {
         Name = "WordPress Server"
     }
@@ -44,6 +46,7 @@ resource "aws_instance" "db_server"{
     ami           = "ami-0b418580298265d5c"
     instance_type = "t2.micro"
     security_groups = [ aws_security_group.security_group.name ]
+    key_name = aws_key_pair.key_pair_db_server.key_name
     tags = {
         Name = "MySQL Server"
     }
@@ -69,4 +72,69 @@ resource "aws_security_group" "security_group" {
   tags = {
     Name = "DevOps project security_group"
   }
+}
+
+#SSH key-pair
+provider tls {
+  version = "~> 2.1"
+}
+
+#key for ci_cd_server
+variable "key_name_ci_cd_server" {
+    type = string
+    default = "key_name_ci_cd_server"
+}
+
+resource "tls_private_key" "key_name_ci_cd_server" {
+    algorithm = "RSA" 
+    rsa_bits = 4096
+}
+resource "aws_key_pair" "key_pair_ci_cd_server" {
+    key_name = var.key_name_ci_cd_server
+    public_key = tls_private_key.key_name_ci_cd_server.public_key_openssh
+}
+
+output "key_name_ci_cd_server" {
+  value = tls_private_key.key_name_ci_cd_server.private_key_pem
+  sensitive = true
+}
+
+#key for wordpress_server
+variable "key_name_wordpress_server" {
+    type = string
+    default = "key_name_wordpress_server"
+}
+
+resource "tls_private_key" "key_name_wordpress_server" {
+    algorithm = "RSA" 
+    rsa_bits = 4096
+}
+resource "aws_key_pair" "key_pair_wordpress_server" {
+    key_name = var.key_name_wordpress_server
+    public_key = tls_private_key.key_name_wordpress_server.public_key_openssh
+}
+
+output "key_name_wordpress_server" {
+  value = tls_private_key.key_name_wordpress_server.private_key_pem
+  sensitive = true
+}
+
+#key for db_server
+variable "key_name_db_server" {
+    type = string
+    default = "key_name_db_server"
+}
+
+resource "tls_private_key" "key_name_db_server" {
+    algorithm = "RSA" 
+    rsa_bits = 4096
+}
+resource "aws_key_pair" "key_pair_db_server" {
+    key_name = var.key_name_db_server
+    public_key = tls_private_key.key_name_db_server.public_key_openssh
+}
+
+output "key_name_db_server" {
+  value = tls_private_key.key_name_db_server.private_key_pem
+  sensitive = true
 }
