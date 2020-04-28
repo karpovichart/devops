4007e04d90f27a493565b832733bb87124f614ba
xbxjsnsb <adverterio@gmail.com>
2020-04-28 15:38:01 +0300

    fixed build for monitoring

--- scripts/orchestration/orchestrating_script.py
+++ scripts/orchestration/orchestrating_script.py
@@ -150,37 +150,42 @@ if select_action=='1':
 
                 print('Start pipeline build for monitoring app')
 
-                status_url = 'http://' + ci_cd_server_public_ip + ':8080/jenkins/job/monitoring/1/consoleText?token=' + token
-                status = requests.get(status_url, auth=HTTPBasicAuth('user', 'password1'))
+                build_url = 'http://' + ci_cd_server_public_ip + ':8080/jenkins/job/monitoring/build?token=' + token
+                build = requests.get(build_url, auth=HTTPBasicAuth('user', 'password1'))
 
-                f2 = open("file2","w")
-                f2.write(status.text)
-                f2.close()
+                while True:
 
-                f1_read = open("file1","r")
-                f1_read_text = f1_read.readlines()
-                f1_read.close()
+                    status_url = 'http://' + ci_cd_server_public_ip + ':8080/jenkins/job/monitoring/1/consoleText?token=' + token
+                    status = requests.get(status_url, auth=HTTPBasicAuth('user', 'password1'))
 
-                f2_read = open("file2","r")
-                f2_read_text = f2_read.readlines()
-                f2_read.close()
+                    f2 = open("file2","w")
+                    f2.write(status.text)
+                    f2.close()
 
-                start = len(f1_read_text)
-                new = f2_read_text[start:]
-                
-                if ''.join(new):
-                    print(''.join(new))
+                    f1_read = open("file1","r")
+                    f1_read_text = f1_read.readlines()
+                    f1_read.close()
 
-                f1_write = open("file1","w")
-                f1_write.write(status.text)
-                f1_write.close()
+                    f2_read = open("file2","r")
+                    f2_read_text = f2_read.readlines()
+                    f2_read.close()
 
-                if 'Finished: SUCCESS' in status.text or 'Finished: FAILURE' in status.text:
-                    print('\n Infrastructure has been successfully deployed \n')
-                    print('\n Jenkins CI/CD server IP: ', ci_cd_server_public_ip)
-                    print('\n WordPress server IP: ', wordpress_server_public_ip)
-                    print('\n Monitoring App: ', ci_cd_server_public_ip + ':5000')
-                    break
+                    start = len(f1_read_text)
+                    new = f2_read_text[start:]
+                    
+                    if ''.join(new):
+                        print(''.join(new))
+
+                    f1_write = open("file1","w")
+                    f1_write.write(status.text)
+                    f1_write.close()
+
+                    if 'Finished: SUCCESS' in status.text or 'Finished: FAILURE' in status.text:
+                        print('\n Infrastructure has been successfully deployed \n')
+                        print('\n Jenkins CI/CD server IP: ', ci_cd_server_public_ip)
+                        print('\n WordPress server IP: ', wordpress_server_public_ip)
+                        print('\n Monitoring App: ', ci_cd_server_public_ip + ':5000')
+                        break
         
     else:
         #set terraform working directory
@@ -300,39 +305,44 @@ if select_action=='1':
 
                 print('Start pipeline build for monitoring app')
 
-                status_url = 'http://' + ci_cd_server_public_ip + ':8080/jenkins/job/monitoring/1/consoleText?token=' + token
-                status = requests.get(status_url, auth=HTTPBasicAuth('user', 'password1'))
+                build_url = 'http://' + ci_cd_server_public_ip + ':8080/jenkins/job/monitoring/build?token=' + token
+                build = requests.get(build_url, auth=HTTPBasicAuth('user', 'password1'))
 
-                f2 = open("file2","w")
-                f2.write(status.text)
-                f2.close()
+                while True:
 
-                f1_read = open("file1","r")
-                f1_read_text = f1_read.readlines()
-                f1_read.close()
+                    status_url = 'http://' + ci_cd_server_public_ip + ':8080/jenkins/job/monitoring/1/consoleText?token=' + token
+                    status = requests.get(status_url, auth=HTTPBasicAuth('user', 'password1'))
 
-                f2_read = open("file2","r")
-                f2_read_text = f2_read.readlines()
-                f2_read.close()
+                    f2 = open("file2","w")
+                    f2.write(status.text)
+                    f2.close()
 
-                start = len(f1_read_text)
-                new = f2_read_text[start:]
-                
-                if ''.join(new):
-                    print(''.join(new))
+                    f1_read = open("file1","r")
+                    f1_read_text = f1_read.readlines()
+                    f1_read.close()
 
-                f1_write = open("file1","w")
-                f1_write.write(status.text)
-                f1_write.close()
+                    f2_read = open("file2","r")
+                    f2_read_text = f2_read.readlines()
+                    f2_read.close()
 
-                if 'Finished: SUCCESS' in status.text or 'Finished: FAILURE' in status.text:
-                    print('\n Infrastructure has been successfully deployed \n')
-                    print('\n Jenkins CI/CD server IP: ', ci_cd_server_public_ip)
-                    print('\n WordPress 1 server IP: ', wordpress_server_1_public_ip)
-                    print('\n WordPress 2 server IP: ', wordpress_server_2_public_ip)
-                    print('\n Load Balancer server IP: ', load_balancer_server_public_ip)
-                    print('\n Monitoring App: ', ci_cd_server_public_ip + ':5000')
-                    break
+                    start = len(f1_read_text)
+                    new = f2_read_text[start:]
+                    
+                    if ''.join(new):
+                        print(''.join(new))
+
+                    f1_write = open("file1","w")
+                    f1_write.write(status.text)
+                    f1_write.close()
+
+                    if 'Finished: SUCCESS' in status.text or 'Finished: FAILURE' in status.text:
+                        print('\n Infrastructure has been successfully deployed \n')
+                        print('\n Jenkins CI/CD server IP: ', ci_cd_server_public_ip)
+                        print('\n WordPress 1 server IP: ', wordpress_server_1_public_ip)
+                        print('\n WordPress 2 server IP: ', wordpress_server_2_public_ip)
+                        print('\n Load Balancer server IP: ', load_balancer_server_public_ip)
+                        print('\n Monitoring App: ', ci_cd_server_public_ip + ':5000')
+                        break
 else:
     os.chdir(terraform_dir)
     subprocess.run(['terraform', 'destroy', '-auto-approve'])
